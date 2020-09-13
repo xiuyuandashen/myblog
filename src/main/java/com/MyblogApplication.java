a23f0e9435babd2f5097b8eec499fea984c5b213
xiuyuandashen <43900059+xiuyuandashen@users.noreply.github.com>
2020-09-13 16:52:44 +0800

    Update MyblogApplication.java

--- src/main/java/com/MyblogApplication.java
+++ src/main/java/com/MyblogApplication.java
@@ -3,7 +3,7 @@ package com;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// 666
 @SpringBootApplication
 @MapperScan("com.dao")
 public class MyblogApplication {
