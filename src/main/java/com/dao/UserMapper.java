3774ea17114e379fabdf754cf60306efe47609bb
zlf <1556450877@qq.com>
2020-12-12 16:10:32 +0800

    漏了，忘记提交

---
+++ src/main/java/com/dao/UserMapper.java
@@ -0,0 +1,23 @@
+package com.dao;
+
+import com.entity.myUser;
+import com.entity.role;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserMapper {
+
+    myUser loadUserByUsername(String name);
+
+    List<role> findRoleByUserId(Integer id);
+
+    myUser selectByUserId(long id);
+
+    int insertUser(myUser user);
+
+    int addUserRole(Integer userId,Integer roleId);
+
+    int blogNumber(Integer id);
+}
